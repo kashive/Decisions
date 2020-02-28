0430931842afe8d0c2ad9fe9bddf2f6b12bb8452
Avishek Neupane <avishek.neupane@gmail.com>
2020-02-28 17:43:35 -0500

    Added panels that scroll and made container take up the entire page

--- src/App.js
+++ src/App.js
@@ -1,80 +1,103 @@
 import React from "react";
 import SideNavInternal from "./SideNavInternal";
 
-import { Container, Header, Content } from "rsuite";
+import { Container, Header, Content, PanelGroup, Panel, Li } from "rsuite";
 
 function App() {
   return (
-    <div className="show-fake-browser sidebar-page">
+    <div>
       <Container>
         <SideNavInternal />
-        <Container style={{ paddingLeft: 30 }}>
-          <Header>
-            <h2>Decisions</h2>
-          </Header>
+        <Container
+          style={{
+            paddingLeft: 80,
+            paddingRight: 80,
+            backgroundColor: "#f8f9fa",
+            minHeight: "100vh"
+          }}
+        >
           <Content>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla
-            pharetra diam sit amet nisl suscipit adipiscing. Sed elementum
-            tempus egestas sed sed risus. Lectus quam id leo in vitae turpis
-            massa sed elementum. Diam sit amet nisl suscipit adipiscing.
-            Consectetur adipiscing elit pellentesque habitant morbi tristique
-            senectus et netus. Volutpat est velit egestas dui id ornare. Viverra
-            accumsan in nisl nisi. Sed augue lacus viverra vitae congue eu
-            consequat ac felis. Nulla pharetra diam sit amet. Nulla posuere
-            sollicitudin aliquam ultrices. Odio euismod lacinia at quis risus
-            sed vulputate. Blandit cursus risus at ultrices. Egestas congue
-            quisque egestas diam in arcu cursus euismod. Ornare arcu dui vivamus
-            arcu felis bibendum ut tristique. Egestas sed tempus urna et
-            pharetra. Consequat nisl vel pretium lectus quam id leo in vitae.
-            Gravida neque convallis a cras semper auctor neque. Faucibus in
-            ornare quam viverra orci. Porttitor lacus luctus accumsan tortor
-            posuere ac ut consequat semper. A diam maecenas sed enim. In
-            fermentum et sollicitudin ac orci phasellus. Consequat ac felis
-            donec et odio pellentesque. Habitant morbi tristique senectus et
-            netus et malesuada fames. Ut sem viverra aliquet eget sit amet. Sem
-            fringilla ut morbi tincidunt augue interdum velit euismod. Elementum
-            sagittis vitae et leo. Et sollicitudin ac orci phasellus egestas.
-            Porttitor leo a diam sollicitudin tempor. Sed enim ut sem viverra
-            aliquet eget sit amet tellus. Enim ut sem viverra aliquet eget. Quam
-            viverra orci sagittis eu. Convallis convallis tellus id interdum
-            velit laoreet id donec. Laoreet id donec ultrices tincidunt arcu.
-            Risus nullam eget felis eget nunc lobortis mattis aliquam faucibus.
-            Id eu nisl nunc mi ipsum. Tellus cras adipiscing enim eu turpis
-            egestas. Non diam phasellus vestibulum lorem sed risus ultricies. At
-            tempor commodo ullamcorper a lacus vestibulum sed arcu. Ipsum dolor
-            sit amet consectetur. Magnis dis parturient montes nascetur
-            ridiculus mus mauris vitae ultricies. In est ante in nibh mauris
-            cursus mattis molestie. Dui sapien eget mi proin. Purus sit amet
-            volutpat consequat mauris nunc congue nisi. Varius vel pharetra vel
-            turpis nunc eget. Malesuada nunc vel risus commodo viverra. A erat
-            nam at lectus urna duis convallis. Elementum facilisis leo vel
-            fringilla est ullamcorper eget nulla. Proin sed libero enim sed
-            faucibus turpis. Pellentesque eu tincidunt tortor aliquam nulla.
-            Augue ut lectus arcu bibendum at varius vel pharetra vel. Faucibus
-            in ornare quam viverra. Sed turpis tincidunt id aliquet risus
-            feugiat in ante. Urna cursus eget nunc scelerisque viverra mauris.
-            Consequat mauris nunc congue nisi vitae suscipit tellus mauris.
-            Aliquet eget sit amet tellus cras adipiscing enim. Ullamcorper
-            dignissim cras tincidunt lobortis feugiat. Nulla pharetra diam sit
-            amet nisl suscipit adipiscing bibendum est. Tincidunt praesent
-            semper feugiat nibh sed pulvinar. Ultricies mi quis hendrerit dolor
-            magna. Nunc faucibus a pellentesque sit amet porttitor. Nisl purus
-            in mollis nunc sed id semper risus. Nulla pharetra diam sit amet
-            nisl. Orci ac auctor augue mauris augue neque. Urna molestie at
-            elementum eu facilisis sed odio morbi. Egestas erat imperdiet sed
-            euismod nisi porta lorem mollis. Velit euismod in pellentesque massa
-            placerat duis ultricies. Habitant morbi tristique senectus et. Sed
-            felis eget velit aliquet sagittis id consectetur. Elit eget gravida
-            cum sociis natoque. Tellus mauris a diam maecenas sed enim ut. In
-            fermentum posuere urna nec tincidunt. Pharetra sit amet aliquam id
-            diam maecenas. Volutpat est velit egestas dui id ornare arcu odio
-            ut. Nibh ipsum consequat nisl vel pretium lectus. Aenean euismod
-            elementum nisi quis eleifend. Dui nunc mattis enim ut tellus
-            elementum sagittis. Sit amet commodo nulla facilisi. Semper risus in
-            hendrerit gravida rutrum. At tellus at urna condimentum. Convallis a
-            cras semper auctor neque vitae tempus quam. Aliquam sem fringilla ut
-            morbi tincidunt.
+            <Header style={{ paddingBottom: "20px", paddingTop: "10px" }}>
+              <h2>Decisions</h2>
+            </Header>
+            <PanelGroup
+              style={{
+                background: "white",
+                height: "85vh",
+                paddingTop: "10px",
+                overflowY: "scroll"
+              }}
+              accordion
+              bordered
+            >
+              <Panel header="Panel 1" defaultExpanded>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Nulla pharetra diam sit amet nisl suscipit adipiscing. Sed
+                elementum tempus egestas sed sed risus. Lectus quam id leo in
+                vitae turpis massa sed elementum. Diam sit amet nisl suscipit
+                adipiscing. Consectetur adipiscing elit pellentesque habitant
+                morbi tristique senectus et netus. Volutpat est velit egestas
+                dui id ornare. Viverra accumsan in nisl nisi. Sed augue lacus
+                viverra vitae congue eu consequat ac felis. Nulla pharetra diam
+                sit amet. Nulla posuere sollicitudin aliquam ultrices. Odio
+                euismod lacinia at quis risus sed vulputate. Blandit cursus
+                risus at ultrices. Egestas congue quisque egestas diam in arcu
+                cursus euismod. Ornare arcu dui vivamus arcu felis bibendum ut
+                tristique. Egestas sed tempus urna et pharetra. Consequat nisl
+                vel pretium lectus quam id leo in vitae. Gravida neque convallis
+                a cras semper auctor neque. Faucibus in ornare quam viverra
+                orci. Porttitor lacus luctus accumsan tortor posuere ac ut
+                consequat semper. A diam maecenas sed enim. In fermentum et
+                sollicitudin ac orci phasellus. Consequat ac felis donec et odio
+                pellentesque.
+              </Panel>
+              <Panel header="Panel 2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Nulla pharetra diam sit amet nisl suscipit adipiscing. Sed
+                elementum tempus egestas sed sed risus. Lectus quam id leo in
+                vitae turpis massa sed elementum. Diam sit amet nisl suscipit
+                adipiscing. Consectetur adipiscing elit pellentesque habitant
+                morbi tristique senectus et netus. Volutpat est velit egestas
+                dui id ornare. Viverra accumsan in nisl nisi. Sed augue lacus
+                viverra vitae congue eu consequat ac felis. Nulla pharetra diam
+                sit amet. Nulla posuere sollicitudin aliquam ultrices. Odio
+                euismod lacinia at quis risus sed vulputate. Blandit cursus
+                risus at ultrices. Egestas congue quisque egestas diam in arcu
+                cursus euismod. Ornare arcu dui vivamus arcu felis bibendum ut
+                tristique. Egestas sed tempus urna et pharetra. Consequat nisl
+                vel pretium lectus quam id leo in vitae. Gravida neque convallis
+                a cras semper auctor neque. Faucibus in ornare quam viverra
+                orci. Porttitor lacus luctus accumsan tortor posuere ac ut
+                consequat semper. A diam maecenas sed enim. In fermentum et
+                sollicitudin ac orci phasellus. Consequat ac felis donec et odio
+                pellentesque.
+              </Panel>
+
+              <Panel header="Panel 3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Nulla pharetra diam sit amet nisl suscipit adipiscing. Sed
+                elementum tempus egestas sed sed risus. Lectus quam id leo in
+                vitae turpis massa sed elementum. Diam sit amet nisl suscipit
+                adipiscing. Consectetur adipiscing elit pellentesque habitant
+                morbi tristique senectus et netus. Volutpat est velit egestas
+                dui id ornare. Viverra accumsan in nisl nisi. Sed augue lacus
+                viverra vitae congue eu consequat ac felis. Nulla pharetra diam
+                sit amet. Nulla posuere sollicitudin aliquam ultrices. Odio
+                euismod lacinia at quis risus sed vulputate. Blandit cursus
+                risus at ultrices. Egestas congue quisque egestas diam in arcu
+                cursus euismod. Ornare arcu dui vivamus arcu felis bibendum ut
+                tristique. Egestas sed tempus urna et pharetra. Consequat nisl
+                vel pretium lectus quam id leo in vitae. Gravida neque convallis
+                a cras semper auctor neque. Faucibus in ornare quam viverra
+                orci. Porttitor lacus luctus accumsan tortor posuere ac ut
+                consequat semper. A diam maecenas sed enim. In fermentum et
+                sollicitudin ac orci phasellus. Consequat ac felis donec et odio
+                pellentesque.
+              </Panel>
+            </PanelGroup>
           </Content>
         </Container>
       </Container>
