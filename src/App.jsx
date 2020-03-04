27a83d5a2d7c6f936caed7367c36f3893cc77f35
Avishek Neupane <avishek.neupane@gmail.com>
2020-03-04 13:42:42 -0500

    Added context section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SideNavInternal from "./SideNavInternal";
 import { Container, Header, Content, PanelGroup, Panel } from "rsuite";
 
 import InlineTextEditWithHighlight from "./InlineTextEditWithHighlight";
+import ContextTextEdit from "./ContextTextEdit";
 
 let classNames = require("classnames");
 
@@ -14,7 +15,8 @@ class App extends React.Component {
       decisions: [
         {
           id: "1",
-          title: "Decision 1"
+          title: "Decision 1",
+          context: "This is why we need to make this decision"
         }
       ],
       currentDecisionId: "1",
@@ -36,12 +38,22 @@ class App extends React.Component {
     );
   }
 
+  //todo: refactor handleTitleChange and handleContextChange
   handleTitleChange = title => {
     const decisions = [...this.state.decisions];
     const currentDecision = this.getCurrentDecision();
     const currentDecisionIndex = decisions.indexOf(currentDecision);
     decisions[currentDecisionIndex] = { ...currentDecision };
     decisions[currentDecisionIndex].title = title;
+    this.setState({ decisions });
+  };
+
+  handleContextChange = context => {
+    const decisions = [...this.state.decisions];
+    const currentDecision = this.getCurrentDecision();
+    const currentDecisionIndex = decisions.indexOf(currentDecision);
+    decisions[currentDecisionIndex] = { ...currentDecision };
+    decisions[currentDecisionIndex].context = context;
     this.setState({ decisions });
   };
 
@@ -98,27 +110,11 @@ class App extends React.Component {
                 accordion
                 bordered
               >
-                <Panel header="Panel 1" defaultExpanded>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Nulla pharetra diam sit amet nisl suscipit adipiscing. Sed
-                  elementum tempus egestas sed sed risus. Lectus quam id leo in
-                  vitae turpis massa sed elementum. Diam sit amet nisl suscipit
-                  adipiscing. Consectetur adipiscing elit pellentesque habitant
-                  morbi tristique senectus et netus. Volutpat est velit egestas
-                  dui id ornare. Viverra accumsan in nisl nisi. Sed augue lacus
-                  viverra vitae congue eu consequat ac felis. Nulla pharetra
-                  diam sit amet. Nulla posuere sollicitudin aliquam ultrices.
-                  Odio euismod lacinia at quis risus sed vulputate. Blandit
-                  cursus risus at ultrices. Egestas congue quisque egestas diam
-                  in arcu cursus euismod. Ornare arcu dui vivamus arcu felis
-                  bibendum ut tristique. Egestas sed tempus urna et pharetra.
-                  Consequat nisl vel pretium lectus quam id leo in vitae.
-                  Gravida neque convallis a cras semper auctor neque. Faucibus
-                  in ornare quam viverra orci. Porttitor lacus luctus accumsan
-                  tortor posuere ac ut consequat semper. A diam maecenas sed
-                  enim. In fermentum et sollicitudin ac orci phasellus.
-                  Consequat ac felis donec et odio pellentesque.
+                <Panel header="Context" defaultExpanded>
+                  <ContextTextEdit
+                    model={decision.context}
+                    handleContextChange={this.handleContextChange}
+                  />
                 </Panel>
                 <Panel header="Panel 2">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
---
+++ src/ContextTextEdit.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import FroalaEditor from "react-froala-wysiwyg";
+
+class ContextTextEdit extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <FroalaEditor
+        model={this.props.model}
+        onModelChange={this.props.handleContextChange}
+        config={{
+          placeholderText:
+            "Why do you need to make this decision? What triggred it? Why is this important?"
+        }}
+      />
+    );
+  }
+}
+
+export default ContextTextEdit;
