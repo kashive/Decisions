6a3dc0e64be6731efb2fb674cf77ced73b7a1601
Avishek Neupane <avishek.neupane@gmail.com>
2020-03-18 16:27:33 -0400

    Adding new structure of the state

--- src/DummyState.js
+++ src/DummyState.js
@@ -1,3 +1,76 @@
+export const NORMALIZED_DUMMY_STATE = {
+  entities: {
+    decisions: {
+      byId: {
+        "1": {
+          id: "1",
+          title: "Decision 1",
+          context: "This is why we need to make this decision",
+          variables: ["2", "3"],
+          options: ["4", "5"]
+        }
+      },
+      allIds: ["1"]
+    },
+    variables: {
+      "2": {
+        id: "1",
+        decisionId: "1",
+        name: "Time",
+        weight: 4,
+        description: "Time and tide waits for none"
+      },
+      "3": {
+        id: "3",
+        decisionId: "1",
+        name: "Money",
+        weight: 8,
+        description:
+          "Money and time and tide waits for none and none again and again."
+      }
+    },
+    options: {
+      "4": {
+        id: "4",
+        decisionId: "1",
+        name: "Option A",
+        description: "Solid option",
+        variableScores: ["6", "7"]
+      },
+      "5": {
+        id: "5",
+        decisionId: "1",
+        name: "Option B",
+        description: "Another Solid option",
+        variableScores: ["6", "7"]
+      }
+    },
+    variableScores: {
+      "6": {
+        id: "6",
+        optionId: "4",
+        variableId: "2",
+        score: 8,
+        reasoning: "because of this"
+      },
+      "7": {
+        id: "7",
+        optionId: "5",
+        variableId: "2",
+        score: 8,
+        reasoning: "because of this"
+      },
+      "8": {
+        id: "8",
+        optionId: "5",
+        variableId: "3",
+        score: 2,
+        reasoning: "because of this"
+      }
+    }
+  }
+};
+
 export const DUMMY_DECISIONS = [
   {
     id: "1",
