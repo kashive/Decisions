6ffa7e84ef91da02c7178a9f63183c272dcc9253
Avishek Neupane <avishek.neupane@gmail.com>
2020-05-29 18:47:26 -0400

    Enabling dropdown on both click and hover so that it is easier to access on mobile as well

--- src/components/shared/CustomDropdown.jsx
+++ src/components/shared/CustomDropdown.jsx
@@ -16,7 +16,7 @@ export const CustomDropdown = ({ config, style, iconText }) => {
       renderTitle={() => {
         return <Icon icon={iconText || "ellipsis-h"} />;
       }}
-      trigger="hover"
+      trigger={["hover", "click"]}
       placement="rightStart"
     >
       {config.map((dropdown) => {
